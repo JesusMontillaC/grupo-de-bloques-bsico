f67657e92730efe8e7239da0820492b7bb4d35b8
JesusMontillaC <134914493+JesusMontillaC@users.noreply.github.com>
2023-06-01 08:52:34 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,38 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        `)
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    basic.clearScreen()
+    basic.pause(200)
+})
